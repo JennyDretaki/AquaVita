03f533daac2a19b004ce548b63ef209cba6cca7a
Τζένη Δρετάκη <dretakieugenia@gmail.com>
2026-07-30 21:06:52 +0300

    Adjust touch sensor activation delay
    Increased the delay for touch sensor activation from 1500ms to 3000ms.

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ export default function App() {
     }),
     useSensor(TouchSensor, {
       activationConstraint: {
-        delay: 1500, // 750ms παρατεταμένο πάτημα στο κινητό
+        delay: 3000, // 750ms παρατεταμένο πάτημα στο κινητό
         tolerance: 8, // Αν το δάχτυλο μετακινηθεί πάνω από 8px στα 750ms, ακυρώνεται το drag και γίνεται scroll
       },
     })
